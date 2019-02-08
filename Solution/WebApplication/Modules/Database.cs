f03aebcf770e75570a0b447b110a98de2703b41b
영진 최 <chyj0318@naver.com>
2019-02-08 13:53:58 +0900

    접속정보 변경 DB

--- Solution/WebApplication/Modules/Database.cs
+++ Solution/WebApplication/Modules/Database.cs
@@ -24,7 +24,7 @@ namespace WebApplication.Modules
         {
             conn = new MySqlConnection();
             conn.ConnectionString
-                = string.Format("server={0};user={1};password={2};database={3};port={4};", "gdc3.gudi.kr", "root", "1234", "gudi", "29002");
+                = string.Format("server={0};user={1};password={2};database={3};port={4};", "192.168.3.154", "root", "1234", "gudi", "3306");
             try
             {
                 conn.Open();
